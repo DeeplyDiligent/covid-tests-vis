f5cf2243ccbae271b34ace413682039d596d46bd
Deep B <bhattacharyya.deep@gmail.com>
2020-09-05 18:23:41 +1000

    node 10

--- pages/api/percentTests.ts
+++ pages/api/percentTests.ts
@@ -1,22 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import useSWR from "swr";
 import {Request, Response} from "express";
 import csv from 'csv-parser'
-import csvFetcher from "../../utils/csvFetcher";
-import * as fs from "fs";
-import http from 'http';
 import moment from "moment";
 
 export default async (req: Request, res: Response) => {
-
   fetch("http://covid-sheets-mirror.web.app/api?cache=true&sheet=1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk&range=Testing!A:F")
     .then(resp => new Promise((resolve, reject) => {
       const results = []
       // @ts-ignore
       resp.body.pipe(csv({
-        mapValues: ({ header, index, value }) => {
-          switch(header) {
+        mapValues: ({header, index, value}) => {
+          switch (header) {
             case 'Date':
               return moment(value, 'DD/MM/YYYY')
             case 'Confirmed cases':
@@ -28,10 +23,10 @@ export default async (req: Request, res: Response) => {
           }
         }
       }))
-      .on('data', (data) => results.push(data))
-      .on('end', () => {
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
           res.send(results)
-      });
-  }));
+        });
+    }));
 
 }
