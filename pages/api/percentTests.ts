7a0b0a77b27aad3b1bce868b19a58c03cb0cc6dd
Deep B <bhattacharyya.deep@gmail.com>
2020-09-19 10:30:27 +1000

    better parsing

--- pages/api/percentTests.ts
+++ pages/api/percentTests.ts
@@ -1,32 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {Request, Response} from "express";
-import csv from 'csv-parser'
+import Papa from 'papaparse';
 import moment from "moment";
+global.XMLHttpRequest = require('xhr2');
 
+const toInt = (string: string) => parseInt(string.replace(/[^0-9 | ^.]/g, ''))
 export default async (req: Request, res: Response) => {
-  fetch("http://covid-sheets-mirror.web.app/api?cache=true&sheet=1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk&range=Testing!A:F")
-    .then(resp => new Promise((resolve, reject) => {
-      const results = []
-      // @ts-ignore
-      resp.body.pipe(csv({
-        mapValues: ({header, index, value}) => {
-          switch (header) {
-            case 'Date':
-              return moment(value, 'DD/MM/YYYY')
-            case 'Confirmed cases':
-              return parseInt(value)
-            case 'Total tested':
-              return parseInt(value)
-            default:
-              return value
-          }
-        }
-      }))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          res.send(results)
-        });
-    }));
-
+  Papa.parse("http://covid-sheets-mirror.web.app/api?cache=true&sheet=1nUUU5zPRPlhAXM_-8R7lsMnAkK4jaebvIL5agAaKoXk&range=Testing!A:F",{
+    download: true,
+    header: true,
+    transform: (value, header) => {
+      switch (header) {
+        case 'Date':
+          return moment(value, 'DD/MM/YYYY')
+        case 'Confirmed cases':
+          return toInt(value)
+        case 'Total tested':
+          return toInt(value)
+        default:
+          return value
+      }
+    },
+    complete: function(results, file) {
+      res.send(results.data)
+      res.status(200).end()
+    }
+  })
 }
---
+++ pages/realestate.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+type RealestateProps = {
+
+};
+const Realestate: React.FunctionComponent<RealestateProps> = ({}) => {
+  return (
+    <div className="w-full max-w-6xl m-auto">
+      hello
+    </div>
+  );
+};
+
+export default Realestate;
